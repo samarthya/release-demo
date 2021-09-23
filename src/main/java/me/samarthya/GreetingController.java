73e54d55141fa57735379f917839f0de8324a59d
Saurabh Sharma <saurabh777@gmail.com>
2021-09-23 18:46:48 +0530

    Added comments

--- src/main/java/me/samarthya/GreetingController.java
+++ src/main/java/me/samarthya/GreetingController.java
@@ -1,17 +1,24 @@
+/**
+ * No License required: You are free to use this code as you wish
+ */
 package me.samarthya;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * It exposes a simple restcontroller listening on `/` which will allow returning
+ * a simple string.
+ */
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
+    /**
+     * GetMethod that responds to the GET '/ 'request
+     * @return A constant string "helloSpring!"
+     */
     @GetMapping("/")
     public String greeting() {
        return "hello Spring!";
--- src/main/java/me/samarthya/StartPoint.java
+++ src/main/java/me/samarthya/StartPoint.java
@@ -1,3 +1,6 @@
+/**
+ * No License required: You are free to use this code as you wish
+ */
 package me.samarthya;
 
 
@@ -9,13 +12,26 @@ import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
 
+
+/**
+ * Main class that is the starting point for the bootstrap application. For more information you can refer to {SpringBootApplication}
+ */
 @SpringBootApplication
 public class StartPoint {
 
+    /**
+     * Hooks the main application
+     * @param args All the arguments that are passed from command line.
+     */
     public static void main(String[] args) {
         SpringApplication.run(StartPoint.class, args);
     }
 
+    /**
+     * Factory method that returns CommandLineRunner
+     * @param ctx ApplicationContext {ApplicationContext}
+     * @return {CommandLineRunner}
+     */
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
